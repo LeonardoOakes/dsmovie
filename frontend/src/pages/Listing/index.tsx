ba5a8b15a651471f9f2eb3a3f83cabdc1079d8f4
Leonardo Oakes <leonardo.oakes@gmail.com>
2022-01-14 20:54:57 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,45 +1,44 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import { BASE_URL } from "utils/requests";
+import axios from "axios";
 
 function Listing() {
+
+    // FORMA ERRADA 
+    axios.get(`${BASE_URL}/movies`).then(response => {
+        console.log(response.data);
+    });
+
+
+
     return (
 
         <>
             <Pagination />
-
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                </div>
-                
-                <div className="row">
+
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                </div>
 
-                <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                </div>
 
-                <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                </div>
 
-                <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
                 </div>
             </div>
-
-
         </>
     )
 }
